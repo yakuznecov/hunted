650adf0d21f82238598666097d9c0de40c90db40
yakuznecov <yakuznecov@mail.ru>
2022-07-25 10:07:16 +0300

    fix card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-function Card({ cards, iconCards }) {
-	return (
-		<>
-			{cards.map((card) => (
-				<div className={styles.container} key={card.id}>
-					<div>
-						<h2 className={styles.title}>{card.title}</h2>
-						<div className={styles.count}>
-							<span className={styles.count_num}>{card.countNum}</span>
-							<span className={styles.count_text}>{card.countText}</span>
-						</div>
-						<div className={styles.sum}>{card.sum} руб.</div>
-						<div className={styles.inner}>
-							<div className={styles.item}>{card.item1}</div>
-							<div className={styles.item}>{card.item2}</div>
-							<div className={styles.item}>{card.item3}</div>
-							<div className={styles.item}>{card.item4}</div>
-							<div className={styles.item}>{card.item5}</div>
-							<div className={styles.item}>{card.item6}</div>
-						</div>
-						<a href='/' className={styles.link}>
-							Ещё {card.link} профессий
-						</a>
-					</div>
+function Card({ cards, iconCard }) {
+	const numToStr = (num, arrText) => {
+		if (num % 10 === 1 && num % 100 !== 11) {
+			return arrText[0];
+		} else if (num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 10 || num % 100 >= 20)) {
+			return arrText[1];
+		}
+		return arrText[2];
+	};
 
-					<div className={styles.icon} style={{ backgroundColor: iconCards.color }} key={iconCards.id}>
-						<img src={iconCards.path} alt='icon' />
-					</div>
+	const textLink = (id) => {
+		if (id === 3) {
+			return 'Все компании';
+		}
+		return `Ещё ${cards.link} профессий`;
+	};
+
+	return (
+		<div className={styles.container} key={cards.id}>
+			<div>
+				<h2 className={styles.title}>{cards.title}</h2>
+				<div className={styles.count}>
+					<span className={styles.count_num}>{cards.total}</span>
+					<span className={styles.count_text}>
+						{numToStr(cards.total, ['вакансия', 'вакансии', 'вакансий'])}
+					</span>
 				</div>
-			))}
-		</>
+				<div className={styles.sum}>{cards.id === 6 ? '' : `${cards.min} - ${cards.max} руб.`}</div>
+				<div className={styles.inner}>
+					<div className={styles.item}>{cards.list[0].title}</div>
+					<div className={styles.item}>{cards.list[1].title}</div>
+					<div className={styles.item}>{cards.list[2].title}</div>
+					<div className={styles.item}>{cards.list[3].title}</div>
+					<div className={styles.item}>{cards.list[4].title}</div>
+					<div className={styles.item}>{cards.list[5].title}</div>
+				</div>
+				<a href='/' className={styles.link}>
+					{textLink(cards.id)}
+				</a>
+			</div>
+
+			<div className={styles.icon} style={{ backgroundColor: iconCard.color }} key={iconCard.id}>
+				<img src={iconCard.path} alt='icon' />
+			</div>
+		</div>
 	);
 }
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,48 +1,222 @@
 import React, { useState } from 'react';
 import styles from './Cards.module.scss';
+import stylesCard from '../Card/Card.module.scss';
 import Card from '../Card/Card';
+import cn from 'classnames';
 
 function Cards() {
 	const cards = [
 		{
 			id: 1,
 			title: 'Вакансии дня',
-			countNum: 24,
-			countText: 'Вакансии',
-			sum: '40 000 - 300 000',
-			item1: 'Администратор площадки',
-			item2: 'Помощник режиссера',
-			item3: 'Звукорежиссер',
-			item4: 'Каскадер',
-			item5: 'Оператор - постановщик',
-			item6: '2-ой режиссер',
+			total: 24,
+			min: '40 000',
+			max: '300 000',
+			list: [
+				{
+					id: 1,
+					title: 'Администратор площадки',
+				},
+				{
+					id: 2,
+					title: 'Помощник режиссера',
+				},
+				{
+					id: 3,
+					title: 'Звукорежиссер',
+				},
+				{
+					id: 4,
+					title: 'Каскадер',
+				},
+				{
+					id: 5,
+					title: 'Оператор - постановщик',
+				},
+				{
+					id: 6,
+					title: '2-ой режиссер',
+				},
+			],
 			link: 18,
 		},
 		{
 			id: 2,
 			title: 'горячие вакансии',
-			countNum: 14,
-			countText: 'Вакансий',
-			sum: '2000 - 35 000',
-			item1: 'Хлопушка',
-			item2: 'Консультант по животным',
-			item3: 'Рабочий на площадку',
-			item4: 'Декоратор',
-			item5: 'Оператор - постановщик',
-			item6: 'Работник сцены',
+			total: 14,
+			min: '2 000',
+			max: '35 000',
+			list: [
+				{
+					id: 1,
+					title: 'Хлопушка',
+				},
+				{
+					id: 2,
+					title: 'Консультант по животным',
+				},
+				{
+					id: 3,
+					title: 'Рабочий на площадку',
+				},
+				{
+					id: 4,
+					title: 'Декоратор',
+				},
+				{
+					id: 5,
+					title: 'Оператор - постановщик',
+				},
+				{
+					id: 6,
+					title: 'Работник сцены',
+				},
+			],
 			link: 8,
+		},
+		{
+			id: 3,
+			title: 'компании дня',
+			total: 108,
+			min: '50 000',
+			max: '270 000',
+			list: [
+				{
+					id: 1,
+					title: 'Базилевс',
+				},
+				{
+					id: 2,
+					title: 'Первый канал',
+				},
+				{
+					id: 3,
+					title: 'Студия Первоцвет',
+				},
+				{
+					id: 4,
+					title: 'Военфильм',
+				},
+				{
+					id: 5,
+					title: '2х2 продакшн',
+				},
+				{
+					id: 6,
+					title: 'Единая Медиа Группа',
+				},
+			],
+		},
+		{
+			id: 4,
+			title: 'вакансии в Москве',
+			total: 1512,
+			min: '75 000',
+			max: '380 000',
+			list: [
+				{
+					id: 1,
+					title: 'Кастинг - директор',
+				},
+				{
+					id: 2,
+					title: 'Художник по костюмам',
+				},
+				{
+					id: 3,
+					title: 'Асс художника по костюмам',
+				},
+				{
+					id: 4,
+					title: 'Каскадер',
+				},
+				{
+					id: 5,
+					title: 'Реквизитор',
+				},
+				{
+					id: 6,
+					title: '2-ой режиссер',
+				},
+			],
+			link: 1506,
+		},
+		{
+			id: 5,
+			title: 'вакансии в спб',
+			total: 968,
+			min: '50 000',
+			max: '290 000',
+			list: [
+				{
+					id: 1,
+					title: 'CG Супервайзер',
+				},
+				{
+					id: 2,
+					title: 'Военный консультант',
+				},
+				{
+					id: 3,
+					title: 'Звукорежиссер',
+				},
+				{
+					id: 4,
+					title: 'Хореограф',
+				},
+				{
+					id: 5,
+					title: 'Оператор - постановщик',
+				},
+				{
+					id: 6,
+					title: 'Асс по реквизиту',
+				},
+			],
+			link: 962,
 		},
 	];
 
 	const iconCards = [
-		{ id: 1, color: '#a6e1bf', path: 'images/rocketLaunch.svg' },
-		{ id: 2, color: '#ffb097', path: 'images/fire.svg' },
+		{ color: '#a6e1bf', path: 'images/rocketLaunch.svg' },
+		{ color: '#ffb097', path: 'images/fire.svg' },
+		{ color: '#a8e8ec', path: 'images/leaf.svg' },
+		{ color: '#bbccc8', path: 'images/buildings.svg' },
+		{ color: '#b0d2c1', path: 'images/maskHappy.svg' },
 	];
 
 	return (
 		<section className={styles.container}>
 			<div className={styles.wrapper}>
-				<Card cards={cards} iconCards={iconCards} />
+				{cards.map((card, index) => (
+					<Card cards={card} iconCard={iconCards[index]} key={card.id} />
+				))}
+
+				<div className={stylesCard.container}>
+					<div>
+						<h2 className={stylesCard.title}>другие города</h2>
+						<div className={cn(stylesCard.count, stylesCard.city)}>
+							<span className={stylesCard.count_num}>125</span>
+							<span className={stylesCard.count_text}>Городов</span>
+						</div>
+						<div className={stylesCard.inner}>
+							<div className={stylesCard.item}>Минск</div>
+							<div className={stylesCard.item}>Сочи</div>
+							<div className={stylesCard.item}>Краснодар</div>
+							<div className={stylesCard.item}>Владивосток</div>
+							<div className={stylesCard.item}>Тверь</div>
+							<div className={stylesCard.item}>Нижний Новгород</div>
+							<div className={stylesCard.item}>Архангельск</div>
+						</div>
+						<a href='/' className={stylesCard.link}>
+							Все города
+						</a>
+					</div>
+
+					<div className={stylesCard.icon} style={{ backgroundColor: '#c2d4ff' }}>
+						<img src='images/globe.svg' alt='icon' />
+					</div>
+				</div>
 			</div>
 		</section>
 	);
