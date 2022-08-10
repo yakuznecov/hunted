54804db351f4c696bc90b3b010cef28fa3d432c9
yakuznecov <yakuznecov@mail.ru>
2022-08-10 23:16:25 +0300

    fix join

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,73 +6,73 @@ import BurgerMenu from 'components/BurgerMenu/BurgerMenu';
 import cn from 'classnames';
 
 function Header({}) {
-    const location = useLocation();
-    const [navbar, setNavbar] = useState(false);
+	const location = useLocation();
+	const [navbar, setNavbar] = useState(false);
 
-    useEffect(() => {
-        const stickyNavbar = () => {
-            if (window.scrollY >= 10) {
-                setNavbar(true);
-            } else {
-                setNavbar(false);
-            }
-        };
+	useEffect(() => {
+		const stickyNavbar = () => {
+			if (window.scrollY >= 10) {
+				setNavbar(true);
+			} else {
+				setNavbar(false);
+			}
+		};
 
-        window.addEventListener('scroll', stickyNavbar);
-        return () => {
-            window.removeEventListener('scroll', stickyNavbar);
-        };
-    }, []);
+		window.addEventListener('scroll', stickyNavbar);
+		return () => {
+			window.removeEventListener('scroll', stickyNavbar);
+		};
+	}, []);
 
-    return (
-        <header className={cn(styles.header, { [styles.active]: navbar })}>
-            <div className={cn(styles.menu, { [styles.active]: navbar })}>
-                <Link to="/" className={styles.logo} alt="Hunted">
-                    <img src="images/logo/logo.svg" alt="logo" />
-                </Link>
-                <nav className={styles.nav}>
-                    <ul className={styles.list}>
-                        <li className={styles.item} onMouseUp={() => window.scrollTo({ top: 0 })}>
-                            <Link to="/" className={styles.title}>
-                                Соискателям
-                            </Link>
-                        </li>
+	return (
+		<header className={cn(styles.header, { [styles.active]: navbar })}>
+			<div className={cn(styles.menu, { [styles.active]: navbar })}>
+				<Link to='/' className={styles.logo} alt='Hunted'>
+					<img src='images/logo/logo.svg' alt='logo' />
+				</Link>
+				<nav className={styles.nav}>
+					<ul className={styles.list}>
+						<li className={styles.item} onMouseUp={() => window.scrollTo({ top: 0 })}>
+							<Link to='/' className={styles.title}>
+								Соискателям
+							</Link>
+						</li>
 
-                        <li className={styles.item} onMouseUp={() => window.scrollTo({ top: 0 })}>
-                            <Link to="/employer" className={styles.title}>
-                                Работодателям
-                            </Link>
-                        </li>
+						<li className={styles.item} onMouseUp={() => window.scrollTo({ top: 0 })}>
+							<Link to='/employer' className={styles.title}>
+								Работодателям
+							</Link>
+						</li>
 
-                        <li className={styles.item}>
-                            <span className={styles.title}>Тарифные планы</span>
-                        </li>
-                    </ul>
+						<li className={styles.item}>
+							<span className={styles.title}>Тарифные планы</span>
+						</li>
+					</ul>
 
-                    <ul className={styles.list}>
-                        <li className={styles.item}>
-                            <NavLink to="/" className={styles.title}>
-                                Войти
-                            </NavLink>
+					<ul className={styles.list}>
+						<li className={styles.item}>
+							<NavLink to='/' className={styles.title}>
+								Войти
+							</NavLink>
 
-                            {location.pathname === '/' && (
-                                <NavLink to={'/'} className={styles.create}>
-                                    Создать резюме
-                                </NavLink>
-                            )}
+							{location.pathname === '/' && (
+								<NavLink to={'/'} className={styles.create}>
+									Создать резюме
+								</NavLink>
+							)}
 
-                            {location.pathname === '/employer' && (
-                                <NavLink to={'/'} className={cn(styles.create, styles.employer)}>
-                                    Разместить вакансию
-                                </NavLink>
-                            )}
-                        </li>
-                    </ul>
-                </nav>
-                <BurgerMenu />
-            </div>
-        </header>
-    );
+							{location.pathname === '/employer' && (
+								<NavLink to={'/'} className={cn(styles.create, styles.employer)}>
+									Разместить вакансию
+								</NavLink>
+							)}
+						</li>
+					</ul>
+				</nav>
+				<BurgerMenu />
+			</div>
+		</header>
+	);
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { render } from "react-dom";
 import App from './App';
 
 
 render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById("root")
 );
